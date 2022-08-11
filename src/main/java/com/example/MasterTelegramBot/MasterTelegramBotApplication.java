a39fc9d1f44e1aa77515f7328f3356d71fcd558a
master1346 <shedrin.yakudza46@gmail.com>
2022-08-12 00:14:04 +0300

    Commit 0

---
+++ src/main/java/com/example/MasterTelegramBot/MasterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.MasterTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MasterTelegramBot/MasterTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MasterTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
